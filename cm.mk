0893b8b3586d9c9fc1d200fbf32a7f5ad6b72930
genesixx <erwanleboucher@gmail.com>
2016-08-19 19:07:59 +0200

    h815 update files from lg h815 20g
    Change-Id: I48eca5d18ca795717f2950fc6c3f2a3109b43d9c

--- cm.mk
+++ cm.mk
@@ -11,5 +11,5 @@ PRODUCT_NAME := cm_h815
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE="g4" \
     PRODUCT_NAME="g4_global_com" \
-    BUILD_FINGERPRINT="lge/p1_global_com/p1:6.0/MRA58K/152940055675e:user/release-keys" \
-    PRIVATE_BUILD_DESC="p1_global_com-user 6.0 MRA58K 152940055675e release-keys"
+    BUILD_FINGERPRINT="lge/p1_global_com/p1:6.0/MRA58K/1620215065b6f:user/release-keys" \
+    PRIVATE_BUILD_DESC="p1_global_com-user 6.0 MRA58K 1620215065b6f release-keys"
