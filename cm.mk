75c02d6277ca0358f829a0ce4bdd56372c2c6027
Erwan Leboucher <erwanleboucher@gmail.com>
2016-11-22 23:50:11 -0800

    h815: remove cm nfc Enhanced
    Change-Id: I51e6d69fc4ccbe0610398ddd2b61f7a0a8576457

--- cm.mk
+++ cm.mk
@@ -3,9 +3,6 @@ $(call inherit-product, device/lge/h815/full_h815.mk)
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-# Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
-
 PRODUCT_NAME := cm_h815
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
