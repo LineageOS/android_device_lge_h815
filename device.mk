89c6717d6e697671db7425feb937e4ffa292d82c
Erwan <erwanleboucher@gmail.com>
2017-09-10 14:42:39 +0200

    h815: update with lg h815 v30a
    Change-Id: Ibb79d9a25a739f7f2bd97ce386afda48fe9618b3

--- device.mk
+++ device.mk
@@ -31,9 +31,5 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:system/etc/audio_platform_info.xml
 
-# Gps
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/gps.conf:system/etc/gps.conf
-
 # common g4
 $(call inherit-product, device/lge/g4-common/g4.mk)
