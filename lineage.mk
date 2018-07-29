0e8688c74a8aef9b45464376aed6a8cf99656b77
ThePiGuy0 <thepiguy0@gmail.com>
2018-07-29 11:32:05 +0100

    h815: Fix BUILD_FINGERPRINT
    Removes vendor mismatch error
    
    Change-Id: Ieb85bb061eaf798e11e535c4d4127a3432a809f3

--- lineage.mk
+++ lineage.mk
@@ -8,5 +8,6 @@ PRODUCT_NAME := lineage_h815
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE="g4" \
     PRODUCT_NAME="g4_global_com" \
-    BUILD_FINGERPRINT="lge/p1_global_com/p1:6.0/MRA58K/152940055675e:user/release-keys" \
     PRIVATE_BUILD_DESC="p1_global_com-user 6.0 MRA58K 152940055675e release-keys"
+
+BUILD_FINGERPRINT := "lge/p1_global_com/p1:6.0/MRA58K/152940055675e:user/release-keys"
