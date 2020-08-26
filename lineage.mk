9e96062de41e7dd691ba8d591cbc4fc578bdf7eb
pavlaras <theodoroupaulos@gmail.com>
2020-08-26 17:11:51 +0300

    h815: Switch from cm to lineage lunch combo
    Change-Id: I1e1a08baf0d8a6b23ddf70cba6900dfad889317e

--- lineage.mk
+++ lineage.mk
@@ -6,7 +6,7 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 # Enhanced NFC
 $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
-PRODUCT_NAME := cm_h815
+PRODUCT_NAME := lineage_h815
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE="g4" \
